66040c9a36cfa5da26eca2389929db30edfe70dd
Omar-Maher-Ahmed <om8523302@gmail.com>
2026-02-07 14:07:29 +0200

    refactor: Consolidate user name fields, add cPassword, and comment out personal details in User entity.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,14 +1,14 @@
 import { UUID } from "crypto"
 
 export class User {
-    firstName: string
-    lastName: string
+    name: string
     email: string
     phone: string
     password: string
+    cPassword: string
     role: string
-    age: number
-    gender: string
-    nationalID: string
+    // age: number
+    // gender: string
+    // nationalID: string
     id?: UUID
 }
