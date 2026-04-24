0094adf4e45436dd64dd584441c5a94b85065909
Omar-Maher-Ahmed <om8523302@gmail.com>
2026-04-24 14:53:23 +0300

    refactor: format main.ts source code, update app listener to bind all interfaces, and modify test email credentials

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,68 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ArgumentsHost, BadRequestException, ExceptionFilter, ValidationPipe } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  BadRequestException,
+  ExceptionFilter,
+  ValidationPipe,
+} from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
 import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
-  app.use(express.json({
-    verify: (req: any, res, buf) => {
-      // check body before processing
-      const body = buf.toString();
-      // if HTML tags
-      if (body.match(/<[^>]*>/g)) {
-        throw new BadRequestException('HTML tags are not allowed');
-      }
-      // if JavaScript events
-      if (body.match(/\bon\w+\s*=/gi)) {
-        throw new BadRequestException('Event handlers are not allowed');
-      }
-      // if javascript protocol
-      if (body.match(/javascript:/gi)) {
-        throw new BadRequestException('JavaScript protocol is not allowed');
-      }
-    }
-  }));
+  app.use(
+    express.json({
+      verify: (req: any, res, buf) => {
+        // check body before processing
+        const body = buf.toString();
+        // if HTML tags
+        if (body.match(/<[^>]*>/g)) {
+          throw new BadRequestException('HTML tags are not allowed');
+        }
+        // if JavaScript events
+        if (body.match(/\bon\w+\s*=/gi)) {
+          throw new BadRequestException('Event handlers are not allowed');
+        }
+        // if javascript protocol
+        if (body.match(/javascript:/gi)) {
+          throw new BadRequestException('JavaScript protocol is not allowed');
+        }
+      },
+    }),
+  );
   app.use(helmet.default());
   app.setGlobalPrefix('api/v1');
 
   // Validation settings
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-    errorHttpStatusCode: 400,
-  }));
-  app.useGlobalFilters(new (class implements ExceptionFilter {
-    catch(exception: any, host: ArgumentsHost) {
-      const ctx = host.switchToHttp();
-      const response = ctx.getResponse();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      errorHttpStatusCode: 400,
+    }),
+  );
+  app.useGlobalFilters(
+    new (class implements ExceptionFilter {
+      catch(exception: any, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse();
 
-      // if Internal Server Error
-      if (exception.status === 500 || !exception.status) {
-        return response.status(400).json({
-          statusCode: 400,
-          message: 'Invalid input format',
-          error: 'Bad Request'
-        });
+        // if Internal Server Error
+        if (exception.status === 500 || !exception.status) {
+          return response.status(400).json({
+            statusCode: 400,
+            message: 'Invalid input format',
+            error: 'Bad Request',
+          });
+        }
+
+        response.status(exception.status).json(exception.response);
       }
-
-      response.status(exception.status).json(exception.response);
-    }
-  })());
+    })(),
+  );
 
   // 2. setup Swagger
   const config = new DocumentBuilder()
@@ -65,7 +76,7 @@ async function bootstrap() {
         bearerFormat: 'JWT',
         in: 'header',
       },
-      'access-token',   
+      'access-token',
     )
     .build();
 
@@ -73,7 +84,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 5001);
+  await app.listen(process.env.PORT ?? 5001, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}/docs`);
 }
 bootstrap();
